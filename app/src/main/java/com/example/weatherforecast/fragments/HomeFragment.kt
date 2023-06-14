69728a40d9ea3698029d21c20525d49b0c58a378
Sizllle <vladislaw.kuzminov2017@gmail.com>
2023-06-15 00:26:54 +0300

    Connect OpenWeather API. Configure current weather display.
    Create OpenWeatherAPI class to abstract the API interaction for retrieving weather data. Create an include layout file weather_current.xml and include it in fragment_home.xml. Configure weather image display based on the received id.

--- app/src/main/java/com/example/weatherforecast/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/weatherforecast/fragments/HomeFragment.kt
@@ -1,60 +1,140 @@
 package com.example.weatherforecast.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
 import com.example.weatherforecast.R
+import com.example.weatherforecast.databinding.FragmentHomeBinding
+import com.example.weatherforecast.network.OpenWeatherAPI
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [HomeFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class HomeFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var binding: FragmentHomeBinding
+    private var sunriseTime: Long = 0
+    private var sunsetTime: Long = 0
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+
+        val apiKey = ""
+        val openWeatherAPI = OpenWeatherAPI(apiKey)
+
+        val latitude = 35.6895
+        val longitude = 139.6917
+        // Отображение текущей даты в заданном формате
+        val currentDate = Date()
+        val dateFormat = SimpleDateFormat("EEEE, dd MMM", Locale.ENGLISH)
+        val formattedDate = dateFormat.format(currentDate)
+        binding.includeWeatherCurrent.tvTodaysDate.text = formattedDate.toString()
+        // Определение единицы измерения для погодных данных
+        val unit = if (true) "metric" else "imperial"
+        // Получение текущей погоды от API OpenWeather
+        openWeatherAPI.getCurrentWeather(latitude, longitude, unit) { response ->
+            // Обработка ответа от API OpenWeather
+            if (response != null) {
+                val currentWeather  = response.getJSONObject("current")
+                val weatherId = currentWeather.getJSONArray("weather").getJSONObject(0).getInt("id")
+                val temperature = currentWeather.getInt("temp")
+                val weatherDescription = currentWeather.getJSONArray("weather").getJSONObject(0).getString("description")
+                val feelsLikeTemperature = currentWeather.getInt("feels_like")
+                val windSpeed = currentWeather.getDouble("wind_speed")
+                sunriseTime = currentWeather.getLong("sunrise")
+                sunsetTime = currentWeather.getLong("sunset")
+                val temperatureUnit = if (true) "\u2103" else "\u2109"
+
+                activity?.runOnUiThread {
+                    val drawableId = getWeatherImageId(weatherId)
+                    binding.includeWeatherCurrent.imWeatherPicture.setImageResource(drawableId)
+                    binding.includeWeatherCurrent.tvTemperatureNow.text = "${temperature}$temperatureUnit"
+                    binding.includeWeatherCurrent.tvWeatherDescription.text = weatherDescription.replaceFirstChar { it.uppercase() }
+                    binding.includeWeatherCurrent.tvFeelsLike.text = getString(R.string.feels_like, "$feelsLikeTemperature$temperatureUnit")
+                    binding.includeWeatherCurrent.tvWindSpeed.text = getString(R.string.wind, windSpeed.toString())
+                }
+            } else {
+                showToast("Error")
+            }
+        }
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment HomeFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            HomeFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    private fun showToast(message: String) {
+        val context = requireContext()
+        activity?.runOnUiThread {
+            Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    // Определение изображения погоды
+    private fun getWeatherImageId(weatherId: Int): Int {
+        val isDaytime = isDaytime() // Определение, является ли текущее время дневным
+        val drawableId = when {
+            isIdInArray(weatherId, R.array.weather_ids_thunderstorm) -> R.drawable.thunderstorm
+            isIdInArray(weatherId, R.array.weather_ids_thunderstorm_with_rain) -> R.drawable.thunderstorm_with_rain
+            isIdInArray(weatherId, R.array.weather_ids_thunderstorm_with_heavy_rain) -> R.drawable.thunderstorm_with_heavy_rain
+            isIdInArray(weatherId, R.array.weather_ids_drizzle) -> R.drawable.drizzle
+            isIdInArray(weatherId, R.array.weather_ids_rain) -> R.drawable.rain
+            isIdInArray(weatherId, R.array.weather_ids_freezing_rain) -> R.drawable.freezing_rain
+            isIdInArray(weatherId, R.array.weather_ids_snow) -> R.drawable.snow
+            isIdInArray(weatherId, R.array.weather_ids_snowfall) -> R.drawable.snowfall
+            isIdInArray(weatherId, R.array.weather_ids_wet_snow) -> R.drawable.wet_snow
+            isIdInArray(weatherId, R.array.weather_ids_fog) -> R.drawable.fog
+            isIdInArray(weatherId, R.array.weather_ids_sand) -> R.drawable.sand
+            isIdInArray(weatherId, R.array.weather_ids_tornado) -> R.drawable.tornado
+            isIdInArray(weatherId, R.array.weather_ids_few_clouds) -> {
+                if (isDaytime) {
+                    R.drawable.few_clouds
+                } else {
+                    R.drawable.few_clouds_night
                 }
             }
+            isIdInArray(weatherId, R.array.weather_ids_scattered_clouds) -> {
+                if (isDaytime) {
+                    R.drawable.scattered_clouds
+                } else {
+                    R.drawable.scattered_clouds_night
+                }
+            }
+            isIdInArray(weatherId, R.array.weather_ids_broken_clouds) -> {
+                if (isDaytime) {
+                    R.drawable.broken_clouds
+                } else {
+                    R.drawable.broken_clouds_night
+                }
+            }
+            isIdInArray(weatherId, R.array.weather_ids_clear) -> {
+                if (isDaytime) {
+                    R.drawable.clear
+                } else {
+                    R.drawable.clear_night
+                }
+            }
+            else -> R.drawable.ic_launcher_foreground // Картинка по умолчанию
+        }
+        return drawableId
     }
+
+    // Проверка, является ли текущее время дневным или ночным
+    private fun isDaytime(): Boolean {
+        // Текущее время в секундах
+        val currentTime = System.currentTimeMillis() / 1000
+        // Выполняется проверка, находится ли текущее время (until) в диапазоне
+        // Между временем восхода (sunriseTime) и заходом солнца (sunsetTime)
+        return currentTime in (sunriseTime until sunsetTime)
+    }
+
+    // Проверка наличия идентификатора в массиве
+    private fun isIdInArray(id: Int, arrayResId: Int): Boolean {
+        val array = resources.getIntArray(arrayResId)
+        return array.contains(id)
+    }
+
 }
---
+++ app/src/main/java/com/example/weatherforecast/network/OpenWeatherAPI.kt
@@ -0,0 +1,42 @@
+package com.example.weatherforecast.network
+
+import android.util.Log
+import okhttp3.*
+import org.json.JSONObject
+import java.io.IOException
+
+class OpenWeatherAPI (private val apiKey: String) {
+
+    /**
+     * Функция для получения текущей погоды
+     * @param latitude Широта
+     * @param longitude Долгота
+     * @param unit Единицы измерения (metric - метрическая система, imperial - американская система)
+     * @param callback Callback-функция, которая будет вызвана после получения ответа от API
+     */
+    fun getCurrentWeather(latitude: Double, longitude: Double, unit: String , callback: (JSONObject?) -> Unit) {
+        val url = "https://api.openweathermap.org/data/3.0/onecall?lat=$latitude&lon=$longitude&units=$unit&exclude={part}&appid=$apiKey"
+
+        val client = OkHttpClient()
+        val request = Request.Builder()
+            .url(url)
+            .build()
+
+        // Отправка асинхронного запроса к API OpenWeather
+        client.newCall(request).enqueue(object : Callback {
+            override fun onResponse(call: Call, response: Response) {
+                val jsonData = response.body?.string()
+                val jsonObject = jsonData?.let { JSONObject(it) }
+                // Вызов callback-функции с полученными данными о погоде
+                callback(jsonObject)
+            }
+
+            override fun onFailure(call: Call, e: IOException) {
+                Log.e("OpenWeatherAPI", "Failed to fetch weather data: ${e.message}")
+                // Вызов callback-функции с параметром null в случае ошибки
+                callback(null)
+            }
+        })
+    }
+
+}
